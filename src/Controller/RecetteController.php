243c740a9cf3a4b7ecab3e49a2408d80dcb962dd
Jean Daniel Boccara <jdboccara@gmail.com>
2020-01-11 15:58:44 +0100

    debut fonction recherche

--- src/Controller/RecetteController.php
+++ src/Controller/RecetteController.php
@@ -58,6 +58,9 @@
     /**
      * @Route("/new", name="recette_new", methods={"GET","POST"})
      * @IsGranted("ROLE_USER")
+     * @param Request $request
+     * @param CategorieRepository $repository
+     * @return Response
      */
     public function new(Request $request, CategorieRepository $repository): Response
     {
@@ -158,6 +161,7 @@
      * @Security("is_granted('ROLE_USER') and user == recette.getAuthor()", message="vous ne pouvez pas modifier les annonces dont vous n'êtes pas l'auteur")
      * @param Request $request
      * @param Recette $recette
+     * @param CategorieRepository $repository
      * @return Response
      */
     public function edit(Request $request , Recette $recette, CategorieRepository $repository): Response
